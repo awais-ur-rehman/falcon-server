4a7c084d23dbfb6f4cc1dfc18caa5b7f29ea639d
awais-urrehman <awais.jarral@issm.ai>
2025-07-18 22:33:41 +0500

    fixed: route

--- src/api/routes/v1/index.ts
+++ src/api/routes/v1/index.ts
@@ -3,6 +3,7 @@ import cookieParser from "cookie-parser";
 import authRoutes from "./auth.route.js";
 import userRoutes from "./user.route.js";
 import roleRoutes from "./role.route.js";
+import announcementRoute from "./announcement.route.js";
 
 
 const router: Router = express.Router();
@@ -13,6 +14,7 @@ router.get("/status", (req, res) => res.send("OK"));
 router.use("/auth", authRoutes);
 router.use("/users", userRoutes);
 router.use("/roles", roleRoutes);
+router.use("/announcement", announcementRoute);
 
 
 export default router;
